7b74df7219ddbd1304592f71a61f22b4e9782176
SahebSandhuSingh <mrsahebsandhu@gmail.com>
2025-12-27 01:29:20 +0530

    Test 2

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,4 +3,4 @@ from model.train import train_model
 def test_model_prediction():
     model = train_model()
     prediction = model.predict([[6]])
-    assert round(prediction[0], 2) == 12
+    assert round(prediction[0], 2) == 12.00
