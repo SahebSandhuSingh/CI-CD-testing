59832d8b3cc095d9c424cf25411020fb01a0f824
SahebSandhuSingh <mrsahebsandhu@gmail.com>
2025-12-27 01:27:48 +0530

    Testing Pipeline

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,5 +2,5 @@ from model.train import train_model
 
 def test_model_prediction():
     model = train_model()
-    prediction = model.predict([[5]])
-    assert round(prediction[0], 2) == 10
+    prediction = model.predict([[6]])
+    assert round(prediction[0], 2) == 12
